75fc4caa12c496661c3418d36128f82d1c6d9408
Michael Thiessen <electromikenetic@gmail.com>
2022-09-26 10:32:43 -0400

    1-1 File-based Routing

---
+++ pages/course.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+  >
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-medium">
+          Course:
+          <span class="font-bold">Mastering Nuxt 3</span>
+        </span>
+      </h1>
+    </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+        <!-- All the lessons for the course listed here -->
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <h2>Lesson</h2>
+        <p>This is a lesson</p>
+      </div>
+    </div>
+  </div>
+</template>
