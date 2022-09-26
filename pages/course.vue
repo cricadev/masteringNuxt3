82ebe50df75cd9f8baadac19835789efcf39ce02
Michael Thiessen <electromikenetic@gmail.com>
2022-09-26 10:53:27 -0400

    1-3 Nested Routes

--- pages/course.vue
+++ pages/course.vue
@@ -22,8 +22,7 @@
       <div
         class="prose p-12 bg-red-100 rounded-md w-[65ch]"
       >
-        <h2>Lesson</h2>
-        <p>This is a lesson</p>
+        <NuxtPage />
       </div>
     </div>
   </div>
---
+++ pages/course/lesson.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+</template>
