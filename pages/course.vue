8c1b6110270251910cb4f1c965bb4f0645198f3a
Michael Thiessen <electromikenetic@gmail.com>
2022-09-26 10:45:03 -0400

    1-2 Displaying Course Lessons

--- pages/course.vue
+++ pages/course.vue
@@ -19,7 +19,9 @@
         <!-- All the lessons for the course listed here -->
       </div>
 
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+      <div
+        class="prose p-12 bg-red-100 rounded-md w-[65ch]"
+      >
         <h2>Lesson</h2>
         <p>This is a lesson</p>
       </div>
