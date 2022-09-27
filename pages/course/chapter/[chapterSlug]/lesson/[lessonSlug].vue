6cef97f0e88e7d286a9a0af960a4ba3520bcd0af
Michael Thiessen <electromikenetic@gmail.com>
2022-09-27 09:15:52 -0400

    1-4 Dynamic Routes

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+</template>
--- pages/course/lesson.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-</template>
