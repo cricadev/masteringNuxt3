7872872733dd61fe02d2d0f3d291a72e67247811
Michael Thiessen <electromikenetic@gmail.com>
2022-09-28 10:21:47 -0400

    1-9 Navigating with NuxtLink

--- pages/course.vue
+++ pages/course.vue
@@ -13,10 +13,27 @@
 
     <div class="flex flex-row justify-center flex-grow">
       <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
+        <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+          >
+            <span class="text-gray-500"
+              >{{ index + 1 }}.</span
+            >
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
       </div>
 
       <div class="prose p-12 bg-white rounded-md w-[65ch]">
@@ -25,3 +42,7 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const { chapters } = useCourse();
+</script>
