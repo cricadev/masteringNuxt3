05dd4126209ca2cdb97afb72ffdbd83651172f25
Michael Thiessen <electromikenetic@gmail.com>
2022-10-11 10:22:14 -0400

    1-11 The useHead Composable

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -43,4 +43,11 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`;
+});
+useHead({
+  title,
+});
 </script>
