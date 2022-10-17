d24bf5ee9f0a6e0530f9de8b92b14a02bdb40b27
Michael Thiessen <electromikenetic@gmail.com>
2022-10-17 10:03:55 -0400

    3-1 Adding in Some TypeScript

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,44 @@
 import courseData from './courseData';
 
-export const useCourse = () => {
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  videoId: number;
+  text: string;
+  sourceUrl?: string;
+};
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+};
+
+type Course = {
+  title: string;
+  chapters: Chapter[];
+};
+
+export const useCourse = (): Course => {
+  const chapters: Chapter[] = courseData.chapters.map(
+    (chapter) => {
+      const lessons: Lesson[] = chapter.lessons.map(
+        (lesson) => ({
+          ...lesson,
+          path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+        })
+      );
+      return {
+        ...chapter,
+        lessons,
+      };
+    }
+  );
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+    chapters,
   };
 };
