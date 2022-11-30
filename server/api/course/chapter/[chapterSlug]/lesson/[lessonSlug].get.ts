f842788603b0e22359d6285a6f95b82f2d0d6e6e
Michael Thiessen <electromikenetic@gmail.com>
2022-11-30 10:10:09 -0500

    5-2 Lesson Endpoint

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,4 +1,29 @@
+import course from '~/server/courseData';
+
 export default defineEventHandler((event) => {
   const { chapterSlug, lessonSlug } = event.context.params;
-  return `Lesson "${lessonSlug}" in chapter "${chapterSlug}"`;
+
+  const chapter = course.chapters.find(
+    (chapter) => chapter.slug === chapterSlug
+  );
+
+  if (!chapter) {
+    throw createError({
+      statusCode: 404,
+      message: 'Chapter not found',
+    });
+  }
+
+  const lesson = chapter.lessons.find(
+    (lesson) => lesson.slug === lessonSlug
+  );
+
+  if (!lesson) {
+    throw createError({
+      statusCode: 404,
+      message: 'Lesson not found',
+    });
+  }
+
+  return lesson;
 });
