0b4cf11cf52d49b73788bb41cc73bc1f6ebb0b22
Michael Thiessen <electromikenetic@gmail.com>
2022-11-30 15:17:49 -0500

    5-4 Course Meta Endpoint

---
+++ server/api/course/meta.get.ts
@@ -0,0 +1,53 @@
+import { Course, Chapter } from '~/types/course';
+import course from '~/server/courseData';
+
+course as Course;
+
+type OutlineBase = {
+  title: string;
+  slug: string;
+  number: number;
+};
+
+type OutlineChapter = OutlineBase & {
+  lessons: OutlineLesson[];
+};
+
+type OutlineLesson = OutlineBase & {
+  path: string;
+};
+
+type CourseMeta = {
+  title: string;
+  chapters: OutlineChapter[];
+};
+
+export default defineEventHandler((event): CourseMeta => {
+  const outline: OutlineChapter[] = course.chapters.reduce(
+    (prev: OutlineChapter[], next: Chapter) => {
+      const lessons: OutlineLesson[] = next.lessons.map(
+        (lesson) => ({
+          title: lesson.title,
+          slug: lesson.slug,
+          number: lesson.number,
+          path: `/course/chapter/${next.slug}/lesson/${lesson.slug}`,
+        })
+      );
+
+      const chapter: OutlineChapter = {
+        title: next.title,
+        slug: next.slug,
+        number: next.number,
+        lessons,
+      };
+
+      return [...prev, chapter];
+    },
+    []
+  );
+
+  return {
+    title: course.title,
+    chapters: outline,
+  };
+});
