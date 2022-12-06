63c6866a898d2b106836103fb3fee6596a2b3ba9
Michael Thiessen <electromikenetic@gmail.com>
2022-12-06 08:16:31 -0500

    5-6 Advanced Data Fetching

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,17 +1,40 @@
+import { StorageSerializers } from '@vueuse/core';
+import { LessonWithPath } from '~~/types/course';
+
 export default async (
   chapterSlug: string,
   lessonSlug: string
 ) => {
-  const { data, error } = await useFetch(
-    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+  // Use sessionStorage to cache the lesson data
+  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+  const lesson = useSessionStorage<LessonWithPath>(
+    url,
+    null,
+    {
+      // By passing null as default it can't automatically
+      // determine which serializer to use
+      serializer: StorageSerializers.object,
+    }
   );
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
+  if (!lesson.value) {
+    const { data, error } = await useFetch<LessonWithPath>(
+      url
+    );
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
+      });
+    }
+
+    lesson.value = data.value;
+  } else {
+    console.log(
+      `Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`
+    );
   }
 
-  return data;
+  return lesson;
 };
