b6a57f438368e88595548669cdc919dbf58bc3a9
Michael Thiessen <electromikenetic@gmail.com>
2022-12-15 15:25:32 -0500

    5-12 Getting Data From Prisma

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,43 +1,23 @@
-import {
-  Course,
-  Chapter,
-  Lesson,
-  LessonWithPath,
-} from '~/types/course';
-import course from '~/server/courseData';
+import { PrismaClient } from '@prisma/client';
 
-course as Course;
+const prisma = new PrismaClient();
 
-export default defineEventHandler(
-  (event): LessonWithPath => {
-    const { chapterSlug, lessonSlug } =
-      event.context.params;
+export default defineEventHandler(async (event) => {
+  const { chapterSlug, lessonSlug } = event.context.params;
 
-    const chapter: Maybe<Chapter> = course.chapters.find(
-      (chapter) => chapter.slug === chapterSlug
-    );
-
-    if (!chapter) {
-      throw createError({
-        statusCode: 404,
-        message: 'Chapter not found',
-      });
-    }
-
-    const lesson: Maybe<Lesson> = chapter.lessons.find(
-      (lesson) => lesson.slug === lessonSlug
-    );
-
-    if (!lesson) {
-      throw createError({
-        statusCode: 404,
-        message: 'Lesson not found',
-      });
-    }
-
-    return {
-      ...lesson,
-      path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
-    };
-  }
-);
+  return prisma.lesson.findFirst({
+    where: {
+      Chapter: {
+        slug: chapterSlug,
+      },
+    },
+    include: {
+      Chapter: {
+        select: {
+          slug: true,
+          title: true,
+        },
+      },
+    },
+  });
+});
