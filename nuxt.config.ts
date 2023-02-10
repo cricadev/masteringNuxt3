920fc4fc49e8b48046961fa885c9f5d7232639bd
Michael Thiessen <electromikenetic@gmail.com>
2023-02-10 09:43:03 -0500

    7-2 Static Generation and Prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
