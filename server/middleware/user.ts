f5b61c51baee7016154c5c9f3bdf01f34b0daa85
Cristian Casallas <cristiancasallas998@gmail.com>
2023-04-11 09:06:12 -0500

    from single to double quote on user.ts middleware

--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -1,4 +1,4 @@
-import { serverSupabaseUser } from '#supabase/server';
+import { serverSupabaseUser } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
   const user = await serverSupabaseUser(event);
